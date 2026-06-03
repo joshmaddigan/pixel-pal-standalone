2b5d879a06d2a55c9518f014b8f6291ea3f1cb6f
Josh Maddigan <josh.maddigan@gmail.com>
2026-06-02 22:36:52 -0400

    add collision detection and autodeath!

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ pygame.init()
 screen_width = 800
 screen_height = 600
 player_size = 50
+x = 100
+y = 100
+player = pygame.Rect(x, y, player_size, player_size)
+enemy = pygame.Rect(200, 150, player_size, player_size)
+
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pixel Pal")
 
 WHITE = (255, 255, 255)
-x = 100
-y = 100
+
 
 running = True
 while running:
@@ -27,8 +31,17 @@ while running:
     if keys[pygame.K_DOWN] and y < screen_height - player_size:
         y += 5
 
+    player.x = x
+    player.y = y
+
     screen.fill(WHITE)
     pygame.draw.rect(screen, (225, 0, 0), (x, y, player_size, player_size))
+    pygame.draw.rect(screen, (0, 0, 255), (200, 150, player_size, player_size))
     pygame.display.flip()
+    clock = pygame.time.Clock()
+    clock.tick(60)
+    if player.colliderect(enemy):
+        print("Collision detected!")
+        running = False
 
 pygame.quit()
