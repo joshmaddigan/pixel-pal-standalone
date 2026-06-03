2ddfcd1ae7017fde650262cd3de8a97f05e89a01
Josh Maddigan <josh.maddigan@gmail.com>
2026-06-03 08:33:38 -0400

    Create Player class and draw method

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ pygame.init()
 screen_width = 800
 screen_height = 600
 player_size = 50
-x = 100
-y = 100
-player = pygame.Rect(x, y, player_size, player_size)
+
 enemy = pygame.Rect(200, 150, player_size, player_size)
 
 screen = pygame.display.set_mode((screen_width, screen_height))
@@ -15,6 +13,20 @@ pygame.display.set_caption("Pixel Pal")
 
 WHITE = (255, 255, 255)
 
+class Player:
+    def __init__(self, x, y, hp, color, size):
+        self.x = x
+        self.y = y
+        self.hp = hp
+        self.color = color
+        self.size = size
+        self.rect = pygame.Rect(self.x, self.y, self.size, self.size)
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
+
+draw_player = Player(100, 100, 100, (0, 0, 255), 50)
+draw_enemy = Player(200, 200, 100, (255, 0, 0), 50)
 
 running = True
 while running:
@@ -22,25 +34,25 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and x > 0:
-        x -= 5
-    if keys[pygame.K_RIGHT] and x < screen_width - player_size:
-        x += 5
-    if keys[pygame.K_UP] and y > 0:
-        y -= 5
-    if keys[pygame.K_DOWN] and y < screen_height - player_size:
-        y += 5
+    if keys[pygame.K_LEFT] and draw_player.x > 0:
+        draw_player.x -= 5
+    if keys[pygame.K_RIGHT] and draw_player.x < screen_width - player_size:
+        draw_player.x += 5
+    if keys[pygame.K_UP] and draw_player.y > 0:
+        draw_player.y -= 5
+    if keys[pygame.K_DOWN] and draw_player.y < screen_height - player_size:
+        draw_player.y += 5
 
-    player.x = x
-    player.y = y
+    draw_player.rect.x = draw_player.x
+    draw_player.rect.y = draw_player.y
 
     screen.fill(WHITE)
-    pygame.draw.rect(screen, (225, 0, 0), (x, y, player_size, player_size))
-    pygame.draw.rect(screen, (0, 0, 255), (200, 150, player_size, player_size))
+    draw_player.draw(screen)
+    draw_enemy.draw(screen)
     pygame.display.flip()
     clock = pygame.time.Clock()
     clock.tick(60)
-    if player.colliderect(enemy):
+    if draw_player.rect.colliderect(draw_enemy):
         print("Collision detected!")
         running = False
 
